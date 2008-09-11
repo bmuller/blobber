a855411b6ca9ab8f36a66753b751a1b45041f2c1
Brian Muller <bamuller@gmail.com>
2008-09-11 19:08:11 +0000

    working on configuration

---
+++ blobber/configuration.h
@@ -0,0 +1,15 @@
+#include "blobber.h"
+
+namespace blobber {
+  class Parser : public Glib::Markup::Parser {
+    void on_start_element(Glib::Markup::ParseContext &context, string name, Glib::Markup::AtributeMap& attr) {
+
+    };
+  }
+
+  class Configuration {
+    Configuration() {
+      BlobberParser parser = 
+    }
+  };
+};
