142ec93a5588217f8d7f618e460819d35d7056a4
Brian Muller <bamuller@gmail.com>
2008-09-18 03:02:45 +0000

    working on pong

--- modules/pong/pong.cpp
+++ modules/pong/pong.cpp
@@ -17,6 +17,7 @@
 */
 
 #include "pong.h"
+#include "math.h"
 
 using namespace std;
 using namespace blobber;
@@ -43,7 +44,7 @@ void Pong::init(Camarea &area, ProjectionWindow &pw) {
 
 
   Glib::Rand rand;
-  ball_direction = rand.get_int_range(0, 360);
+  ball_direction = rand.get_int_range(0, 180);
   moving_right = true;
   ball.x = area.width / 2;
   ball.y = area.height / 2;
@@ -51,18 +52,36 @@ void Pong::init(Camarea &area, ProjectionWindow &pw) {
 
 
 void Pong::update(Camarea &area, ProjectionWindow &pw) {
-  pw.draw_circle(ball, 5, BLACK);
+  cout << ball.x << " " << ball.y << endl;
+  pw.draw_circle(ball, 6, BLACK);
+  int movement = 10;
   if(ball.x <= 0) {
     moving_right = true;
-    ball.x += 5;
+    ball.x += movement;
+    if(fabs(ball_direction - 270) <= 5)
+      ball_direction += 10;
   } else if(ball.x >= area.width) {
     moving_right = false;
-    ball.x -= 5;
+    ball.x -= movement;
+    if(fabs(ball_direction - 90) <= 5)
+      ball_direction += 10;
   } else if(moving_right) {
-    ball.x += 5;
+    ball.x += movement;
   } else {
-    ball.x -= 5;
+    ball.x -= movement;
   }
+
+  if(ball.y <= 0 || ball.y >= area.height) {
+    ball_direction = 360 - ball_direction;
+  }
+
+  if(moving_right) {
+    ball_direction = (ball_direction > 90) ? (ball_direction-90) : (90 - ball_direction);
+  } else {
+    ball_direction = (ball_direction > 270) ? (ball_direction - 270) : (270 - ball_direction);
+  }
+  ball.y = atan(ball_direction) * (180/M_PI);
+
   pw.draw_circle(ball, 5, BLUE);  
 };
 
