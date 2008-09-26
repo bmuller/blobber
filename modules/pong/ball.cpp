2626af52156eb5b5a542b0abf2fe64fb2b0425b9
Semmy <semmy@cs.cofc.edu>
2008-09-26 19:28:19 +0000

    added all the necessary files this time.

---
+++ modules/pong/ball.cpp
@@ -0,0 +1,37 @@
+#include "ball.h"
+#include <iostream>
+
+using namespace std;
+
+Ball::Ball()  {
+  pos.x = 0;
+  pos.y = 0;
+  x_dir = 1;
+  y_dir = 1;
+  speed = 5;
+  diameter = 5;
+  court_height = 250;
+  court_width = 150;
+}
+
+void Ball::move()  {
+  pos.x += x_dir*speed;
+  if(pos.x >= court_width || pos.x <= 0)
+    x_dir = -x_dir;
+
+  pos.y += y_dir*speed;
+  if(pos.y >= court_height || pos.y <= 0)
+    y_dir = -y_dir;
+}
+
+void Ball::printInfo()  {
+  cout << pos.x << " " << pos.y  <<endl;
+}
+
+void Ball::setCourtHeight(int h)  {
+  court_height = h;
+}
+
+void Ball::setCourtWidth(int w)  {
+  court_width = w;
+}
---
+++ modules/pong/ball.h
@@ -0,0 +1,24 @@
+#include "blobber.h"
+
+using namespace blobber;
+
+class Ball  {
+ public:
+  Ball();
+
+  void move();
+  void setCourtHeight(int h);
+  void setCourtWidth(int w);
+  void printInfo();
+
+  /**I probably should make these private eventually**/
+  COORD pos;
+  short int x_dir;
+  short int y_dir;
+  short int speed;
+  short int diameter;
+  int court_height;
+  int court_width;
+
+};
+
