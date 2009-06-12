b2d427733a8d4249b0d0c38d0d7c4156dedc1b07
Matt Shotwell <shotwelm@musc.edu>
2009-06-12 21:15:52 +0000

    pointer updates

--- modules/pointer/pointer.cpp
+++ modules/pointer/pointer.cpp
@@ -20,6 +20,9 @@ void Pointer::init(Camarea &area, ProjectionWindow &pw) {
 
 
 void Pointer::update(Camarea &area, ProjectionWindow &pw) {
+  // this branch is for future extensions, i.e. how clicks should be
+  // made. POINTER_MODE_TIMED means that a click signal is sent when 
+  // the pointer has not been moved for one second
   switch( mod ) {
     case POINTER_MODE_TIMED : update_mode_timed( area, pw ); break;
   }
@@ -35,24 +38,29 @@ void Pointer::update_mode_timed( Camarea &area, ProjectionWindow &pw ) {
     XTestFakeMotionEvent( dpy, DefaultScreen( dpy ), t.x, t.y, CurrentTime );
 
     //Haven't got the clicking part down yet
+    string delString, clkString;
+    num_to_string( loc.distance_from( poi[0].coord ), delString );
+    num_to_string( ( (float) ( clock() - clk ) ) / CLOCKS_PER_SEC, clkString );
+    debug( "pointer: delta = " + delString + " clock = " + clkString );
 
-    // if new location is  > 15 away, start clock over, reset loc
-    //if( loc.distance_from( poi[0].coord ) > 15.0 ) {
-    //  clk = clock();
-    //  loc.copy( poi[0].coord );
-    //}
+    // if new location is  > 20 away, start clock over, reset loc
+    if( loc.distance_from( poi[0].coord ) > 20.0 ) {
+      clk = clock();
+      loc.copy( poi[0].coord );
+    }
 
-    // if 1.0 seconds have passed and pointer has not moved, send click
-    //if( ( (float) ( clock() - clk ) ) / CLOCKS_PER_SEC > 1.0 ) {
-    //  XTestFakeButtonEvent( dpy, Button1, True, CurrentTime );
-    //  XTestFakeButtonEvent( dpy, Button1, False, CurrentTime );
-    //  clk = clock();
-    //}
+    // if 1.0 seconds have passed and pointer has not moved (much), send click
+    if( ( (float) ( clock() - clk ) ) / CLOCKS_PER_SEC > 1.0 ) {
+      XTestFakeButtonEvent( dpy, Button1, True, CurrentTime );
+      XTestFakeButtonEvent( dpy, Button1, False, CurrentTime );
+      clk = clock();
+    }
 
     XCloseDisplay( dpy );
   }
 }
 
+/// Converts camera coordinates to screen coordinates
 void Pointer::translate_coordinates( Camarea &area, COORD &camera, COORD &screen ) {
   screen.x = (int) ( ( ( (float) camera.x ) / ( (float) area.dimensions.width ) ) * scw );
   screen.y = (int) ( ( ( (float) camera.y ) / ( (float) area.dimensions.height ) ) * sch );
